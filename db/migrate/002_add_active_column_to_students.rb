4821a2a0a221195bcad14ff8fb57a27eef51942d
Michelle Olivieri <michelleo72@gmail.com>
2018-05-30 15:29:34 +0000

    automatically backed up by learn

--- db/migrate/002_add_active_column_to_students.rb
+++ db/migrate/002_add_active_column_to_students.rb
@@ -1,7 +1,5 @@
 class AddActiveColumnToStudents < ActiveRecord::Migration
   def change
-    add_column :students do |t|
-      t.string :students, :active, :boolean, default: false
-    end
+    add_column :students, :active, :boolean, default: false
   end
 end
